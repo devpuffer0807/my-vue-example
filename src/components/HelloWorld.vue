25dbdfdda5be2f47bae04f866abf28b1f6cfc257
PufferDev0807 <pufferdev0807@gmail.com>
2021-11-24 16:32:21 +0300

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,3 +19,25 @@ export default {
   }
 }
 </script>
+
+
+
+// <template>
+//   <component v-bind:is="view"></component>
+// </template>
+
+// <script>
+// export default {
+//   data: () => {
+//     return {
+//       view: 'component1'
+//     }
+//   },
+//   components: {
+//     component1: {
+//       template: '<h1>HELLO</h1>'
+//     }
+//   }
+// }
+// </script>
+
