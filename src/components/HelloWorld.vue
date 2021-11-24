a70921a3e63d5333388aa40774d7d0ea02641c84
PufferDev0807 <pufferdev0807@gmail.com>
2021-11-24 16:23:50 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>First Name</span>
+    <input type="text" v-model="firstname" />
+    <span>Last Name</span>
+    <input type="text" v-model="lastname" />
+    <div>
+      <h1>{{ firstname }} {{ lastname }}</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      firstname: 'Puffer',
+      lastname: 'Dev'
+    }
+  }
+}
+</script>
